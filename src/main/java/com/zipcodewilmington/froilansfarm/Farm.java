e5f8369dda9e960c93a91c8f727dd33332a4514d
will <will@zipcodes-MBP-8.fios-router.home>
2020-11-27 21:11:21 -0500

    began adding arrayLists

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -1,5 +1,14 @@
 package com.zipcodewilmington.froilansfarm;
 
+import java.util.ArrayList;
+
 public class Farm {
     //Need to setup arrayLists for farmHouse, chickenCoop, and Stable depending on how many are required
+    Farmhouse farmhouse = new Farmhouse();
+    ArrayList<Stable> stables = new ArrayList<Stable>();
+    ArrayList<ChickenCoop> chickenCoops = new ArrayList<ChickenCoop>();
+
+    public Farm(){
+        //Farm constructor will go here
+    }
 }
