135bec03f24fc08b973a0e7fe37cfbc9430b7248
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-27 21:44:03 -0500

    Updated Chicken and Egg

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
@@ -1,11 +1,16 @@
 package com.zipcodewilmington.froilansfarm.Animal;
+
 import com.zipcodewilmington.froilansfarm.Edible;
 import com.zipcodewilmington.froilansfarm.Egg;
 import com.zipcodewilmington.froilansfarm.Produce;
 
 public class Chicken extends Animal implements Produce {
-    public void eat() {
+    public void eat(Edible earCorn) {
+        for (int i = 0; i < 3; i++) {
+            // ArrayList<EarCorn> corn = new ArrayList<>();
+            //   EarCorn.corn.size()-1;
 
+        }
     }
 
     public String makeNoise() {
@@ -14,6 +19,8 @@ public class Chicken extends Animal implements Produce {
 
     public Edible yield() {
         Edible egg = new Egg();
+        // ArrayList<Egg> eggInventory = new ArrayList;
+        // eggInventory.add(egg);
         return egg;
     }
 }
