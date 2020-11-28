1f74ff55c3360a327843c894936313fdb6331af9
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-27 21:56:03 -0500

    Chicken & Horse tests

--- src/test/java/com/zipcodewilmington/froilansfarm/TestChicken.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/TestChicken.java
@@ -1,8 +1,11 @@
 package com.zipcodewilmington.froilansfarm;
 
 import com.zipcodewilmington.froilansfarm.Animal.Chicken;
+import com.zipcodewilmington.froilansfarm.Animal.Horse;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class TestChicken {
     @Test
@@ -14,5 +17,20 @@ public class TestChicken {
         String actualNoise = chicken.makeNoise();
         //then
         Assert.assertEquals(expectedNoise, actualNoise);
+    }
+
+    @Test
+    public void eatTest(){
+        //given
+        Chicken chicken = new Chicken();
+        ArrayList<EarCorn> cornArray = new ArrayList<EarCorn>();
+        EarCorn corn = new EarCorn();
+        int expectedCornAmount = 0;
+        //when
+        cornArray.add(corn);
+        chicken.eat();
+        //int actualCornAmount = EarCorn.size();
+        //then
+        //   Assert.assertEquals(expectedCornAmount, actualCornAmount);
     }
 }
