81ca87aaf24d11e0f131bd5bf8f0667d00fee84f
nikki <nikki@Gbrads-Mac.local>
2020-11-27 20:09:35 -0500

    Basic Structure

--- src/main/java/com/zipcodewilmington/froilansfarm/Person.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Person.java
@@ -1,6 +1,6 @@
 package com.zipcodewilmington.froilansfarm;
 
-public class Person {
+public class Person implements Eater, NoiseMaker{
     // will implement eater and noisemaker
     String name;
 
