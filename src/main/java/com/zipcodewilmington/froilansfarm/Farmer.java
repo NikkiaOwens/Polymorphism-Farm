969f2fed82515daddfcd3d2d266f27d7520f3b5e
Christian Pirhalla <chrispir@udel.edu>
2020-11-28 17:45:50 -0500

    getters

--- src/main/java/com/zipcodewilmington/froilansfarm/Farmer.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farmer.java
@@ -1,5 +1,24 @@
 package com.zipcodewilmington.froilansfarm;
 
-public abstract class Farmer extends Person implements Rider, Botanist, Pilot{
+public class Farmer extends Person implements Rider, Botanist, Pilot{
+    public void plant() {
+
+    }
+
+    public void eat() {
+
+    }
+
+    public String makeNoise() {
+        return null;
+    }
+
+    public void mount() {
+
+    }
+
+    public void dismount() {
+
+    }
     //abstract?
 }
