65d27d3beb03002347277e0dd59dfced30e6eee2
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-27 20:37:11 -0500

    Added methods to Chicken

--- src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
@@ -1,7 +1,18 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class Chicken extends Animal implements Produce {
+    public void eat(){
+
+    }
+    public String makeNoise(){
+        return "cluck";
+    }
+
     public Edible yield() {
         return null;
+    }
+
+    public void hasBeenFertilized(){
+        //boolean
     }
 }
