44ccd1ad4b65fbe42860de66342cd9f8b6a1aa07
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-28 19:48:33 -0500

    Added Edible parameter to Eater eat()

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Horse.java
@@ -4,7 +4,7 @@ import com.zipcodewilmington.froilansfarm.Edible;
 import com.zipcodewilmington.froilansfarm.Rideable;
 
 public class Horse extends Animal implements Rideable {
-    Boolean isBeingRidden;
+    public Boolean isBeingRidden;
 
     public void eat(Edible earCorn) {
         for (int i = 0; i < 3; i++) {
